bdf6de6248173ca3c0bf9dce51b9eb98cea5b712
Gary Moore <madmangaz@gmail.com>
2023-08-13 14:50:48 +0100

    feat: add ability to change dns server as arg

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,27 @@ use dns_weekend::{query::Query, question::QType};
 struct Args {
     /// The domain name to resolve
     #[clap(short, long)]
-    address: String,
+    addr: String,
+    #[clap(long, default_value_t = String::from("1.1.1.1"))]
+    dns_addr: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let query: Vec<u8> = Query::build_query(&args.address, QType::A);
+    let query: Vec<u8> = Query::build_query(&args.addr, QType::A);
 
-    let addr = "127.0.0.1:12345";
-    let socket = UdpSocket::bind(addr)?;
-    println!("Listening on {}", addr);
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
 
-    let remote_addr: SocketAddr = "8.8.8.8:53".parse()?;
-    println!("{:?}", query);
+    let remote_addr: SocketAddr = format!("{}:53", &args.dns_addr).parse()?;
+    println!("{:02X?}", query);
     socket.send_to(&query, remote_addr)?;
     println!("Sent data to {}", remote_addr.to_string());
 
     let mut buffer = [0u8; 1024];
-    let (size, sender_addr) = socket.recv_from(&mut buffer)?;
+    let (size, _) = socket.recv_from(&mut buffer)?;
 
-    println!(
-        "Received {} bytes from {}: {}",
-        size,
-        sender_addr,
-        String::from_utf8_lossy(&buffer[..size])
-    );
+    println!("Received {} bytes: {:02X?}", size, &buffer[..size]);
 
     return Ok(());
 }
